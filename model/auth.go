6725875396cef1a7f3d155f1482032056a282180
Alfiya <alfiya96960@gmail.com>
2025-02-07 18:50:58 +0530

    make model for connection user roles and permissions

--- model/auth.go
+++ model/auth.go
@@ -2,33 +2,32 @@ package model
 
 type User struct {
 	Base
-	Username    string `json:"username" gorm:"unique" validate:"required,email"`
-	Password    string `json:"password" validate:"required,min=8,max=128"`
-	IsValidated bool   `json:"isValidate" validate:"default:false"`
-	Roles       []Role `json:"roles" gorm:"many2many:user_roles;"`
+	Username    string     `json:"username" gorm:"unique" validate:"required,email"`
+	Password    string     `json:"password" validate:"required,min=8,max=128"`
+	IsValidated bool       `json:"isValidate" validate:"default:false"`
+	Roles       []UserRole `gorm:"foreignKey:UserID"`
 }
 
 type Role struct {
 	Base
-	Name        string       `json:"name" gorm:"size:50;not null;uniqueIndex"`
-	Description string       `json:"description" gorm:"type:text;default:null"`
-	Permissions []Permission `json:"permissions" gorm:"many2many:role_permissions;"`
-	Users       []User       `json:"users" gorm:"many2many:user_roles;"`
+	Name        string     `json:"name" gorm:"size:50;not null;uniqueIndex"`
+	Description string     `json:"description" gorm:"type:text;default:null"`
+	Users       []UserRole `gorm:"foreignKey:RoleID"`
 }
 
 type Permission struct {
 	Base
-	Name        string `gorm:"size:100;not null;unique"`
-	Description string `gorm:"type:text"`
-	Roles       []Role `json:"roles" gorm:"many2many:role_permissions;"`
+	Name        string     `gorm:"size:100;not null;unique"`
+	Description string     `gorm:"type:text"`
+	UserRoles   []UserRole `gorm:"foreignKey:PermissionID"`
 }
 
 type UserRole struct {
-	UserID string `json:"userId" gorm:"primaryKey;size:36"`
-	RoleID string `json:"roleId" gorm:"primaryKey;size:36"`
-}
-type RolePermission struct {
 	Base
-	RoleID       string `json:"roleId" gorm:"primaryKey;size:36"`
-	PermissionID string `json:"permissionId" gorm:"primaryKey;size:36"`
+	UserID       string     `gorm:"not null"`
+	User         User       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	RoleID       string     `gorm:"not null"`
+	Role         Role       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	PermissionID string     `gorm:"not null"`
+	Permission   Permission `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
