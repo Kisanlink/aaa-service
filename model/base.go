180fe558db105b71ad1aa9d018ab4bfe3f8d663e
Alfiya-dev <alfiya@kisanlink.in>
2025-03-24 12:44:54 +0530

    minor changes

--- model/base.go
+++ model/base.go
@@ -7,20 +7,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// type Base struct {
-// 	ID        uuid.UUID `json:"id" "gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-// 	CreatedAt time.Time `json:"createdAt" gorm:"column:created_at"`
-// 	UpdatedAt time.Time `json:"updatedAt" gorm:"column:updated_at"`
-// }
-
-// func (b *Base) BeforeCreate(tx *gorm.DB) (err error) {
-// 	if b.ID == uuid.Nil {
-// 		b.ID = uuid.New()
-// 	}
-// 	return
-// }
-
-// Base model for common fields
 type Base struct {
     ID        string    `gorm:"type:varchar(36);primaryKey"` // Use string for ID
     CreatedAt time.Time `json:"createdAt" gorm:"column:created_at"`
